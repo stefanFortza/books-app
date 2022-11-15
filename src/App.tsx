704cc2f9bb0a1026108d2195ba6c08f38d64183e
Tacu Darius Stefan <tacustefan74@gmail.com>
2022-11-15 18:43:37 +0200

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Navigation from './routes/navigation/navigation';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return <Navigation />
+
+}
+
+export default App;
