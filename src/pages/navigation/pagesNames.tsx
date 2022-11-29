346e4a5709d11bb3ec59bd53e6cbfc18d6539126
Tacu Stefan <tacustefan74@gmail.com>
2022-11-29 12:56:33 +0200

    commit before updating typescript version

---
+++ src/pages/navigation/pagesNames.tsx
@@ -0,0 +1,17 @@
+export enum PageNames {
+  Root = "/",
+  Books = "books",
+  AddBook = "add",
+  EditBook = "edit/:bookId",
+  ShowBook = "show/:bookId",
+  Auth = "auth",
+}
+
+export enum AbsolutePageNames {
+  // Root = PageNames.Root,
+  Books = "books",
+  AddBook = "add",
+  EditBook = "edit/:bookId",
+  ShowBook = "show/:bookId",
+  Auth = "auth",
+}
